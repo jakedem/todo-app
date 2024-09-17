9b397e04fe2f0c496ff30da69392639d31e7f958
Jerry Anane <jerryanane12@gmail.com>
2024-09-17 17:50:36 +0000

    Add delete functionality for todos
    Description:
    - Implemented `deleteTodoItem` to remove specific todos from the list.
    - Added delete button event listener within `createTodoItem`.
    - Updated todo list and localStorage after deleting a todo.

--- app.js
+++ app.js
@@ -40,16 +40,11 @@ function createTodoItem(todo, todoIndex) {
       height="24px"
       viewBox="0 -960 960 960"
       width="24px"
-      fill="#e8eaed"
-    >
+      fill="#e8eaed">
       <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
     </svg>
   </label>
-
-  <label class="todo-text" for="${todoId}"
-    >${todo}</label
-  >
-
+  <label class="todo-text" for="${todoId}">${todo}</label>  
   <button class="delete-button">
     <svg
       fill="var(--secondary-color)"
@@ -57,14 +52,23 @@ function createTodoItem(todo, todoIndex) {
       height="24px"
       viewBox="0 -960 960 960"
       width="24px"
-      fill="#e8eaed"
-    >
+      fill="#e8eaed">
       <path
-        d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
-      />
+        d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
     </svg>
   </button>`;
+
+  const deleteButton = todoLi.querySelector(".delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteTodoItem(todoIndex);
+  });
   return todoLi;
+}
+
+function deleteTodoItem(todoIndex) {
+  allTodos = allTodos.filter((_, i) => i !== todoIndex);
+  saveTodos();
+  updateTodoList();
 }
 
 function saveTodos() {
